e2a9f730bd66dbb604db428861f4686febe5d502
iTech Reset Electronics <itechresetelectronics@iTechs-MacBook-Air.local>
2023-07-17 21:13:34 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import axios from 'axios';
 import Spinner from 'react-bootstrap/Spinner';
 import Navigation from './components/Navigation';
 
-// ABIs
+// ABIs 
 import NFT from './abis/NFT.json'
 
 // Config
@@ -17,10 +17,99 @@ import config from './config.json';
 function App() {
   const [provider, setProvider] = useState(null)
   const [account, setAccount] = useState(null)
+  const [nft, setNFT] = useState(null)
+
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState(null);
+  const [url, setUrl] = useState(null);
+
+  const [message, setMessage] = useState("")
+  const [isWaiting, setIsWaiting] = useState(false)
+  
+
 
   const loadBlockchainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     setProvider(provider)
+    const network = await provider.getNetwork()
+    const nft = new ethers.Contract(config[network.chainId].nft.address, NFT, provider)
+    setNFT(nft)
+    
+ 
+
+  } 
+ 
+  const submitHandler =async (e) => {
+    e.preventDefault()
+     if (name === "" || name === "" ){
+      window.alert("please provide namd and description")
+      return
+     }
+
+    setIsWaiting(true)
+    const imageData = createImage();
+    const url = await uploadImage();
+    await mintImage(url)
+    setIsWaiting(false)
+    setMessage("")
+
+    console.log("Success")
+
+    
+  }
+
+  const createImage = async () => {
+    setMessage("Generating Image...")
+
+    const URL = 'https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2';
+    const response = await axios({
+      url: URL,
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_HUGGING_FACE_API_KEY}`,
+        Accept: 'application/json',
+        'Content-Type' : 'application/json'
+      },
+      data: JSON.stringify({inputs: description, options: {wait_for_model: true},
+       }),
+      responseType: 'arraybuffer',
+    })
+
+    const type = response.headers['content-type']
+    const data = response.data
+
+    const base64data = Buffer.from(data).toString('base64')
+    const img = 'data:${type};base64,' + base64data
+    setImage(img)
+
+    return data
+  }
+
+  const uploadImage = async (imageData) => {
+    setMessage("Uploading Image...")
+
+   const nftstorage = new NFTStorage({ token: process.env.REACT_APP_NFT_STORAGE_API_KEY})
+
+   const {ipnft} = await nftstorage.store({
+    image: new File([imageData], "image.jpeg", {type: "image/jpeg"}),
+    name: name,
+    description: description,
+   })
+
+   const url = `https://ipfs.io/ipfs/${ipnft}/metadata.json`
+   setUrl(url)
+
+   return url
+  }
+
+  const mintImage = async (tokenURI) => {
+    setMessage("waiting for mint..")
+
+    const signer = await provider.getSigner()
+    const transaction = await nft.connect(signer).mint(tokenURI, 
+    {value: ethers.utils.parseUnits("2", "ether")}) 
+    await transaction.wait()
   }
 
   useEffect(() => {
@@ -30,10 +119,31 @@ function App() {
   return (
     <div>
       <Navigation account={account} setAccount={setAccount} />
-      <p>Edit App.js to get started.</p>
-
+      <div className="form">
+        <form onSubmit={submitHandler}>
+          <label>Add your prompt</label>
+          <input type="text" placeholder ="Create a name" onChange={(e) => {setName(e.target.value)}}></input>
+          <input type="text" placeholder ="Create a description" onChange={(e) => {setDescription(e.target.value)}}></input>
+          <input type="submit" value="Create and Mint"></input>
+        </form>
+        <div className='image'>
+          {!isWaiting && image ? (
+            <img src={image} alt="AI generated image"/> 
+          ): isWaiting ? (
+            <div>
+            <Spinner animation="border"/>
+            <p>{message}</p>
+            </div>
+          ) : (
+            <></>
+          )}
+         </div>
+      </div>
+      {!isWaiting && url && (
+        <p>View&nbsp;<a href={url} target="_blank"
+        rel="noreferrer"></a>Metadata</p>
+      )}
     </div>
-  );
-}
+  );} 
 
 export default App;
